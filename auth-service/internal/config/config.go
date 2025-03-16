f5f0c863274fcb810aaa7206683483c05664b578
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-03-16 19:31:22 +0300

    refactor(auth-service/config): move out functions to separate files

--- auth-service/internal/config/config.go
+++ auth-service/internal/config/config.go
@@ -2,11 +2,7 @@ package config
 
 import (
 	"crypto/ed25519"
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"fmt"
-	"os"
 	"path/filepath"
 	"strings"
 	"time"
@@ -72,82 +68,4 @@ func loadConfig(configPath string) (*Config, error) {
 	}
 
 	return &cfg, nil
-}
-
-func validateConfig() error {
-	errorMessageTemplate := "no '%s' keyword found in config"
-
-	if err := validateNestedYaml(errorMessageTemplate, "database", []string{"host", "user", "password", "dbname", "sslmode"}); err != nil {
-		return err
-	}
-
-	if err := validateNestedYaml(errorMessageTemplate, "jwt_token", []string{"public_key_filepath", "private_key_filepath"}); err != nil {
-		return err
-	}
-
-	if err := validateSingleKeyword(errorMessageTemplate, "domain_name"); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func validateSingleKeyword(errorMessageTemplate string, keyword string) error {
-	if !viper.IsSet(keyword) {
-		return fmt.Errorf(errorMessageTemplate, keyword)
-	}
-	return nil
-}
-
-func validateNestedYaml(errorMessageTemplate string, topLevelKeyword string, requiredChildren []string) error {
-	if !viper.IsSet(topLevelKeyword) {
-		return fmt.Errorf(errorMessageTemplate, topLevelKeyword)
-	}
-	for _, child := range requiredChildren {
-		keywordFullName := fmt.Sprintf("%s.%s", topLevelKeyword, child)
-		if !viper.IsSet(keywordFullName) {
-			return fmt.Errorf(errorMessageTemplate, keywordFullName)
-		}
-	}
-	return nil
-}
-
-func loadJwtKeys(jwtConfig *JwtTokenConfig) error {
-	privData, err := os.ReadFile(jwtConfig.PrivateKeyPemFilepath)
-	if err != nil {
-		return fmt.Errorf("error reading private key file: %w", err)
-	}
-	privBlock, _ := pem.Decode(privData)
-	if privBlock == nil {
-		return errors.New("failed to decode PEM block from private key file")
-	}
-	parsedKey, err := x509.ParsePKCS8PrivateKey(privBlock.Bytes)
-	if err != nil {
-		return fmt.Errorf("failed to parse ED25519 private key: %w", err)
-	}
-	ed25519Priv, ok := parsedKey.(ed25519.PrivateKey)
-	if !ok {
-		return errors.New("provided private key is not an ED25519 key")
-	}
-	jwtConfig.Ed25519PrivateKey = ed25519Priv
-
-	pubData, err := os.ReadFile(jwtConfig.PublicKeyPemFilepath)
-	if err != nil {
-		return fmt.Errorf("error reading public key file: %w", err)
-	}
-	pubBlock, _ := pem.Decode(pubData)
-	if pubBlock == nil {
-		return errors.New("failed to decode PEM block from public key file")
-	}
-	pubInterface, err := x509.ParsePKIXPublicKey(pubBlock.Bytes)
-	if err != nil {
-		return fmt.Errorf("failed to parse ED25519 public key: %w", err)
-	}
-	ed25519Pub, ok := pubInterface.(ed25519.PublicKey)
-	if !ok {
-		return errors.New("provided public key is not an ED25519 key")
-	}
-	jwtConfig.Ed25519PublicKey = ed25519Pub
-
-	return nil
 }
