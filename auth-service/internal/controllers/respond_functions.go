c1c4760ac550009c155ec2f9ec1adba54903df2f
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-03-20 16:43:22 +0300

    feat(auth-service/controllers): now response with jwt in body, cookie removed

--- auth-service/internal/controllers/respond_functions.go
+++ auth-service/internal/controllers/respond_functions.go
@@ -18,3 +18,10 @@ func respondWithSuccess(ctx *gin.Context, code int, message string, logger zerol
 	})
 	logger.Info().Msg(message)
 }
+
+func respondWithAccessToken(ctx *gin.Context, code int, access_token string) {
+	ctx.JSON(code, gin.H{
+		"message":      "Successfully logged in",
+		"access_token": access_token,
+	})
+}
