6052e5c9ea060ff24f0326c337ef7f3433cc309b
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-05-01 18:44:00 +0300

    feat(game-service): handle activity creation

--- game-service/internal/amqp_handlers/configure_handlers.go
+++ game-service/internal/amqp_handlers/configure_handlers.go
@@ -1,11 +1,16 @@
 package amqp_handlers
 
 import (
+	"game-metrics/game-service/internal/amqp"
 	"game-metrics/game-service/internal/config"
 
 	"github.com/rs/zerolog"
 )
 
 func ConfigureHandlers(amqpConfig config.AMQPConfig, logger zerolog.Logger) {
+	if err := amqp.RunHandler("activity-service", "game-service", "activity created", amqpConfig.Timeout, handleActivityCreated, logger); err != nil {
+		logger.Fatal().Err(err).Msg("Failed to run activity-created handler")
+	}
+
 	logger.Info().Msg("AMQP handlers started")
 }
---
+++ game-service/internal/repository/activity.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"errors"
+	"fmt"
+	"game-metrics/game-service/internal/models"
+
+	"github.com/google/uuid"
+)
+
+var ErrActivityAccessDenied = errors.New("user does not have access to this activity")
+
+func CreateActivity(id uint, userId uuid.UUID) error {
+	db, err := connectToDatabase()
+	if err != nil {
+		return err
+	}
+
+	activity := models.Activity{Id: id, UserId: userId}
+	if result := db.Create(&activity); result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func ValidateActivityOwner(userId uuid.UUID, activityId uint) error {
+	db, err := connectToDatabase()
+	if err != nil {
+		return err
+	}
+
+	var activity models.Activity
+	result := db.Where("id = ?", activityId).First(&activity)
+	if result.Error != nil {
+		return fmt.Errorf("failed to get activity from database: %w", result.Error)
+	}
+
+	if userId != activity.UserId {
+		return ErrActivityAccessDenied
+	}
+
+	return nil
+}
