bf18361251fcae4421e07beaee93b027ef4e265e
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-05-02 08:17:54 +0300

    fix(game-service): message routing to wrong handler

--- game-service/internal/amqp_handlers/configure_handlers.go
+++ game-service/internal/amqp_handlers/configure_handlers.go
@@ -8,11 +8,11 @@ import (
 )
 
 func ConfigureHandlers(amqpConfig config.AMQPConfig, logger zerolog.Logger) {
-	if err := amqp.RunHandler("activity-service", "game-service", "activity created", amqpConfig.Timeout, handleActivityCreated, logger); err != nil {
+	if err := amqp.RunHandler("activity-service", "game-service-activity-created", "activity created", amqpConfig.Timeout, handleActivityCreated, logger); err != nil {
 		logger.Fatal().Err(err).Msg("Failed to run activity created handler")
 	}
 
-	if err := amqp.RunHandler("players-service", "game-service2", "player-created", amqpConfig.Timeout, handlePlayerCreated, logger); err != nil {
+	if err := amqp.RunHandler("players-service", "game-service-player-created", "player-created", amqpConfig.Timeout, handlePlayerCreated, logger); err != nil {
 		logger.Fatal().Err(err).Msg("Failed to run player-created handler")
 	}
 
